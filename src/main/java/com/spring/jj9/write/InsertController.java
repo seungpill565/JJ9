64fcdb3977b651ed27693c60970babb74cf6225b
junhyoki <driuy89@naver.com>
2022-08-29 11:39:10 +0900

    연습중

--- src/main/java/com/spring/jj9/write/InsertController.java
+++ src/main/java/com/spring/jj9/write/InsertController.java
@@ -1,11 +1,18 @@
 package com.spring.jj9.write;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-@RestController
+@Controller
 public class InsertController {
 	
+	@GetMapping(value = "/sample")
+	public String getPizza() {
+		
+		
+		return "write/insertTalent";
+	}
 }
